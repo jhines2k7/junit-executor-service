84a9669699e8a4af6869b9aab67859ab7c30bd32
James Hines <jameshines10@gmail.com>
2019-09-19 21:52:55 -0500

    Initial commit

---
+++ src/main/java/com/hines/james/junitexecutorservice/JunitExecutorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hines.james.junitexecutorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitExecutorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitExecutorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hines/james/junitexecutorservice/JunitExecutorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hines.james.junitexecutorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitExecutorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
