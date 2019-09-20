19f760d5c64a26c46ab3b97b3c33dfe9a7fea52e
James Hines <jameshines10@gmail.com>
2019-09-20 06:49:33 -0500

    Refactors constructor by removing injected repositories

--- src/main/java/com/hines/james/junitexecutorservice/services/OmniClobDeleterService.java
+++ src/main/java/com/hines/james/junitexecutorservice/services/OmniClobDeleterService.java
@@ -2,8 +2,6 @@ package com.hines.james.junitexecutorservice.services;
 
 import com.hines.james.junitexecutorservice.config.DataSourceProperties;
 import com.hines.james.junitexecutorservice.config.DynamicDataSourceConfiguration;
-import com.hines.james.junitexecutorservice.data.repositories.OmFavoritesRepository;
-import com.hines.james.junitexecutorservice.data.repositories.OmniMetaDataRepository;
 import com.hines.james.junitexecutorservice.messaging.AuthoringObsoleteMessage;
 import com.hines.james.junitexecutorservice.multitenancy.DynamicDataSource;
 import lombok.extern.slf4j.Slf4j;
@@ -16,25 +14,18 @@ public class OmniClobDeleterService {
     private DataSourceProperties dataSourceProperties;
     private DynamicDataSource dynamicDataSource;
     private ClobDeleterRepositoryService clobDeleterRepositoryService;
-    private OmFavoritesRepository omFavoritesRepository;
-    private OmniMetaDataRepository omniMetaDataRepository;
 
     public OmniClobDeleterService(ClobDeleterRepositoryService clobDeleterRepositoryService,
                                   DataSourceProperties dataSourceProperties,
-                                  DynamicDataSource dynamicDataSource,
-                                  OmFavoritesRepository omFavoritesRepository,
-                                  OmniMetaDataRepository omniMetaDataRepository) {
+                                  DynamicDataSource dynamicDataSource) {
         this.dataSourceProperties = dataSourceProperties;
         this.dynamicDataSource = dynamicDataSource;
         this.clobDeleterRepositoryService = clobDeleterRepositoryService;
-        this.omFavoritesRepository = omFavoritesRepository;
-        this.omniMetaDataRepository = omniMetaDataRepository;
     }
 
     public void deleteOmniMetaData(AuthoringObsoleteMessage message) {
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-        clobDeleterRepositoryService = new ClobDeleterRepositoryService(omFavoritesRepository, omniMetaDataRepository);
 
         for(DynamicDataSourceConfiguration dataSourceConfiguration : dataSourceProperties.getDynamicDataSourceConfigurations()) {
             dynamicDataSource.setDataSourceName(dataSourceConfiguration.getName());
